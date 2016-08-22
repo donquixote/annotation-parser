898dceaf1a9b0e0b81338c222537d80f0a205ef4
Andreas Hennings <andreas@dqxtech.net>
2016-08-22 20:56:04 +0200

    Add RawPhpDocAnnotation.

---
+++ src/RawAnnotation/RawPhpDocAnnotation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Donquixote\Annotation\RawAnnotation;
+
+class RawPhpDocAnnotation {
+
+  /**
+   * @var string
+   */
+  private $name;
+
+  /**
+   * @var string|null
+   */
+  private $text;
+
+  /**
+   * @param string $name
+   * @param string|null $text
+   */
+  public function __construct($name, $text) {
+    $this->name = $name;
+    $this->text = $text;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @return string|null
+   */
+  public function getText() {
+    return $this->text;
+  }
+
+}
