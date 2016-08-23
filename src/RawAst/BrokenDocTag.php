262280e64530e165fcf3ac30207b8dde25c56952
Andreas Hennings <andreas@dqxtech.net>
2016-08-23 22:58:31 +0200

    Add BrokenDocTag.

---
+++ src/RawAst/BrokenDocTag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Donquixote\Annotation\RawAst;
+
+class BrokenDocTag {
+
+  /**
+   * @var string
+   */
+  private $name;
+
+  /**
+   * @var string
+   */
+  private $text;
+
+  /**
+   * @param string $name
+   * @param string $text
+   */
+  public function __construct($name, $text) {
+    $this->name = $name;
+    $this->text = $text;
+  }
+
+  /**
+   * @return string
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @return string
+   */
+  public function getText() {
+    return $this->text;
+  }
+
+}
