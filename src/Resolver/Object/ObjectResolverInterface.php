101c79d11c7317bf7399e90dd546d10863549436
Andreas Hennings <andreas@dqxtech.net>
2017-08-29 02:44:10 +0200

    Improve doc comment on ObjectResolverInterface.

--- src/Resolver/Object/ObjectResolverInterface.php
+++ src/Resolver/Object/ObjectResolverInterface.php
@@ -6,11 +6,17 @@ interface ObjectResolverInterface {
 
   /**
    * @param string $name
+   *   Original annotation tag name, before any alias replacement.
    * @param mixed[] $arguments
    *   Arguments which are already resolved.
    * @param \Reflector $reflector
+   *   Class, method or property where the annotation was found.
+   *   It could also be an (empty) CustomReflector, for cases where we only know
+   *   the annotation itself, but not where it was found.
    *
    * @return mixed
+   *
+   * @throws \Donquixote\Annotation\Exception\ResolverException
    */
   public function resolve($name, array $arguments, \Reflector $reflector);
 
